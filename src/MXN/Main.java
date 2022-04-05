5c750b74f1600a6e24d5cd888cc5a70da8c02d92
superman <squatforrest@gmail.com>
2022-04-05 08:54:35 -0500

    Третиӣ коммит

--- src/MXN/Main.java
+++ src/MXN/Main.java
@@ -4,22 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Задание 1
+
         int y = 0;
         while (y < 10) {
             y++;
             System.out.println(y);
         }
-        int i = 10;
-        for (i = 10; i >= 0; i--) {
+        for (int i = 10; i >= 0; i--) {
             System.out.println(i);
         }
+
+        // Задание 2
+
         for (int x = 1; x < 31; x = x + 7) {
             System.out.println("Сегодня пятница " + x + " -е число"); }
-            for (int j = 1896; j < 2122; j = j + 79) {
-                System.out.println("Пролёт кометы " + j);
-        }
-        for (int year = 1822; year <= 2122; year = year + 79) {
-            System.out.println("dates " + year);
+
+        // Задание 3
+
+        int currentYear = 2022;
+        int fromYearStar = currentYear - 200;
+        int lastYear = currentYear + 100;
+
+        for (int i = 0; i < lastYear; i = i + 79) {
+            if (i > fromYearStar && i < lastYear)
+                System.out.println("Пролёт кометы " + i); }
         }
     }
-}
